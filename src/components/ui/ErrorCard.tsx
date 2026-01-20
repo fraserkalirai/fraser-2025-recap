7cdf2796a76f49b26bb30ad012fc2a2662c99f95
fraser <fraserkalirai@gmail.com>
2026-01-20 10:10:41 +0000

    textcolour3

--- src/components/ui/ErrorCard.tsx
+++ src/components/ui/ErrorCard.tsx
@@ -1,7 +1,7 @@
 export default function ErrorCard({ className = '' }: { className?: string }) {
   return (
     <div className={`flex items-center justify-center h-full ${className}`}>
-      <p className="text-md sm:text-lg font-semibold">Error loading data</p>
+      <p className="text-md sm:text-lg font-semibold text-blue-900 dark:text-white">Error loading data</p>
     </div>
   );
 }
--- src/components/ui/NoDataCard.tsx
+++ src/components/ui/NoDataCard.tsx
@@ -1,7 +1,7 @@
 export default function NoDataCard({ className = '' }: { className?: string }) {
   return (
     <div className={`flex items-center justify-center h-full ${className}`}>
-      <p className="text-md sm:text-lg font-semibold">No data available</p>
+      <p className="text-md sm:text-lg font-semibold text-blue-900 dark:text-white">No data available</p>
     </div>
   );
 }
